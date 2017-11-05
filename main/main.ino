42082eab6988afe4333c5b3a1d799ecf893df624
Jonah Spear <Jonahspear512@gmail.com>
2017-11-05 11:15:48 -0500

    Mimic going forward until we are too close to buoy

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,12 @@
 /****************************************************************************
- * Robot Koi Fish Main Code
- * Fundamentals of Robotics Koi Fish Team
+   Robot Koi Fish Main Code
+   Fundamentals of Robotics Koi Fish Team
  ****************************************************************************/
 
 #include<Servo.h>
 #include<SoftwareSerial.h>
+#include<SPI.h>
+#include<Pixy.h>
 
 // Define constants
 #define PRINT true
@@ -22,11 +24,16 @@
 
 #define BATT_MAX 1000             //9 V
 #define BATT_MIN 0                //6 V
+#define CLOSE_THRESH 15000
 
+// Define variables
+int target_sig = 1;
 byte yaw_limits[] = {20, 165};    //Servo limits 20, 165
 byte pitch_limits[] = {21, 165};  //Servo limits 21, 165
+float yaw_servo_pos = 165.0/20.0;
 
-// Define servos
+// Define servos and cam
+Pixy pixy;
 Servo servo_yaw;
 Servo servo_pitch;
 
@@ -39,6 +46,7 @@ void setup() { // ----------S----------S----------S----------S----------S-------
   servo_yaw.attach(SERVO_YAW_PIN);
   servo_pitch.attach(SERVO_PITCH_PIN);
   pinMode(LED_PIN, OUTPUT);
+  pixy.init();
 
   // Open serial comms
   Serial.begin(9600);
@@ -46,7 +54,7 @@ void setup() { // ----------S----------S----------S----------S----------S-------
   // Activate motor power
   digitalWrite(ESTOP_RELAY_PIN, HIGH);
 
-  while(true) {
+  while (true) {
     if (gotInput(121)) {
       println("MSG: Starting robot");
       return;
@@ -54,16 +62,56 @@ void setup() { // ----------S----------S----------S----------S----------S-------
   }
 }
 
-void loop() {
+void loop() { // ----------L----------L----------L----------L----------L----------L----------L----------L----------L----------L
   // Wait for XBee verification to start tests
-    println("Running");
-    if (gotInput(121)) {
-      println("MSG: Estop");
-      stop();
+  if (gotInput(121)) {
+    println("MSG: Estop");
+    stop();
+  }
+  else {
+    // Sense ---------------------------
+    uint16_t blocks = pixy.getBlocks();
+
+    // Think ---------------------------
+    // Get the largest block
+    if (blocks) {
+      int max_area = 0;
+      int max_x = 0;
+      int max_y = 0;
+      for (int j = 0; j < blocks; j++) {
+        if (pixy.blocks[j].signature == target_sig) {
+          // Save the largest block
+          if (pixy.blocks[j].width * pixy.blocks[j].height > max_area) {
+            max_area = pixy.blocks[j].width * pixy.blocks[j].height;
+            max_x = pixy.blocks[j].x;
+            max_y = pixy.blocks[j].y;
+          }
+        }
+
+      }
+      Serial.print("area: ");
+      Serial.print(max_area);
+      Serial.print(" ,x: ");
+      Serial.print(max_x);
+      Serial.print(" ,y: ");
+      Serial.println(max_y);
+
+      if((max_area != 0) && (max_x != 0) && (max_y !=0)) {
+        // Calculate the output tsteering angle
+        yaw_servo_pos = (140.0 / 313.0) * max_x + 20.0;
+      }
+      if (max_area > CLOSE_THRESH) {
+        stop();
+      }
+
+      // Act  ----------------------------
+//      Serial.print("Writing ");
+//      Serial.print(servo_pos);
+//      Serial.println(" to the servo.");
+
+      servo_yaw.write(yaw_servo_pos);
     }
-    else {
-      
-    }
+  }
 }
 
 bool gotInput(int asciiVal) {
@@ -81,11 +129,11 @@ bool gotInput(int asciiVal) {
 }
 
 /*
- * function eStop()
- * DESC: Estops the robot
- * ARGS: none
- * RTNS: none
- */
+   function eStop()
+   DESC: Estops the robot
+   ARGS: none
+   RTNS: none
+*/
 void stop() {
   digitalWrite(LED_PIN, HIGH);
   digitalWrite(ESTOP_RELAY_PIN, LOW);
@@ -100,15 +148,15 @@ void stop() {
 }
 
 void print(char *text) {
-  #ifdef PRINT
+#ifdef PRINT
   Serial.print(text);
-  #endif
+#endif
 }
 
 void println(char *text) {
-  #ifdef PRINT
+#ifdef PRINT
   Serial.println(text);
-  #endif
+#endif
 }
 
 
